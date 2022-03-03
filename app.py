7eea1933415cfbf6756fd01f4790e704bc64c9ac
Sahrial Ihsani Ishak <67180039+sahrialihsani@users.noreply.github.com>
2022-03-03 23:33:56 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# Flask utils
+from flask import Flask, request, render_template
+
+# Define a flask app
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # app.run(host="0.0.0.0",debug=True,port="4100")
+    app.run(debug=True)
